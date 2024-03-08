35c87aa525cd58a060423762ac20f2b59a77d918
sarastatish <mutisomutinda23@gmail.com>
2024-03-08 03:56:11 +0300

    first commit

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num = 40;
+  if (num > 10) {
+    print("your number is greater than 10");
+  } else if (num < 10) {
+    print("your number is less than 10");
+  } else {
+    print("your number is equal to 10");
+  }
+}
